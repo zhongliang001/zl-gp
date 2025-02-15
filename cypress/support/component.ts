9f46fc4d50b6214231bef2fb9bfc6180e1a757ae
zl <coolzhongliang@126.com>
2025-02-15 22:20:59 +0800

    配置测试

---
+++ cypress/support/component.ts
@@ -0,0 +1,10 @@
+// Import commands.js using ES2015 syntax:
+import './commands'
+
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
+
+import { mount } from 'cypress/vue'
+// Ensure global styles are loaded
+
+Cypress.Commands.add('mount', mount)
